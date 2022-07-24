84bbc1f9d4fc98bd8fcf8007af45e4469d4a84d5
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-07-24 13:24:40 -1000

    Build initial component structure

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { AppRegistry } from 'react-native';
+// import { Provider as PaperProvider } from 'react-native-paper';
+import Dashboard from "./Views/Dashboard";
+
+//Libraries
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Dashboard />
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
@@ -17,5 +25,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    border: "2px solid red",
   },
 });
