ac4fc0635124c3b0232dab1e794d0a14e62bf7c8
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-07-26 18:10:18 -0700

    Refactor PickImage and PickImageStyles. Bug fix haptics web error.

--- components/PickImage/PickImage.js
+++ components/PickImage/PickImage.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
-import Constants from 'expo-constants'
+import { Text, View, Image } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as Haptics from 'expo-haptics';
 import {styles} from './PickImageStyles'
@@ -11,8 +10,23 @@ import { Button } from "react-native-paper"
 export default function PickImage() {
   const [localUri, setUri] = useState("")
 
+  //TESTS
+  //END TESTS
+
+
     let openImagePickerAsync = async () => {
+
+
+
+// FIX HAPTICS WEB ERROR
       Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
+        .catch(error => {
+          return
+        })
+// END HAPTICS
+
+
+
       let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
       if(!permissionResult.granted) {
         alert('Access to camera roll is required for the app to function as intended')
@@ -24,10 +38,13 @@ export default function PickImage() {
       }
       setUri(pickerResult.uri)
       }
-
   return (
     <View style={styles.container}>
-      <View>{!localUri ? <Text>Please select an image to continue.</Text> :
+      <View>
+        {!localUri ? 
+        <Text>
+          Please select an image to continue.
+        </Text> :
         <Image
           style={styles.thumbnail}
           source={{uri:localUri}}
--- components/PickImage/PickImageStyles.js
+++ components/PickImage/PickImageStyles.js
@@ -1,5 +1,4 @@
 import { StyleSheet} from 'react-native'
-import React from 'react'
 
 export const styles = StyleSheet.create({
   container: {
