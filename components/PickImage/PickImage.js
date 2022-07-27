eb8f5d31a9635d808e80fc8a66278328ea65f03e
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-07-26 18:16:29 -0700

    Remove unused code. Slight refactor

--- components/PickImage/PickImage.js
+++ components/PickImage/PickImage.js
@@ -3,30 +3,16 @@ import { Text, View, Image } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as Haptics from 'expo-haptics';
 import {styles} from './PickImageStyles'
-
 //Libraries
 import { Button } from "react-native-paper"
 
 export default function PickImage() {
   const [localUri, setUri] = useState("")
-
-  //TESTS
-  //END TESTS
-
-
     let openImagePickerAsync = async () => {
-
-
-
-// FIX HAPTICS WEB ERROR
       Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
         .catch(error => {
           return
         })
-// END HAPTICS
-
-
-
       let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
       if(!permissionResult.granted) {
         alert('Access to camera roll is required for the app to function as intended')
