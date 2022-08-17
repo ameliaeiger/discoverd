2a596644d2f98d0c6adc5fe1bb70e0f86e130735
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-08-16 18:57:04 -0600

    Remove all unused code. Refactor for consistency with ES6 code and style guidelines

--- App.js
+++ App.js
@@ -1,36 +1,30 @@
+//IMPORTS
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, View, Text, ScrollView, Dimensions } from 'react-native';
+import { View, Dimensions } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+//COMPONENTS
 import Dashboard from "./Views/Dashboard";
 import Welcome from './Views/Welcome';
-import {NavigationContainer} from "@react-navigation/native"
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ResponsePage from './Views/ResponsePage'
-import PlantImage from './components/ImageDisplay/PlantImage'
 import HeaderLogo from './components/Header'
 
-export default function App() {
-  const [imageUris, addImageUris] = useState([])
-  const [allImages, setAllImages] = useState([])
+export default App = () => {
+  const [imageBaseStrings, setImageBaseStrings] = useState([])
+  const [imageUris, setImageUris] = useState([])
   const [apiKey, setApiKey] = useState('')
 
   const windowDimensions = Dimensions. get('window')
   const Stack = createNativeStackNavigator();
 
   const handleChange = (baseString, uri) => {
-    console.log("APP 21 BASE STRING", baseString)
-    console.log("APP 22 URI", uri)
-    addImageUris([...imageUris, baseString])
-    // console.log("IMAGE URIS (App.js 22)", imageUris)
-    getView(uri)
+    setImageBaseStrings([...imageBaseStrings, baseString])
+    setImageUris([...imageUris, uri])
   }
 
-  const getView = (uri) => {
-    // console.log("ALL IMAGES (App.js 27)", allImages)
-    setAllImages([...allImages, <PlantImage key={Date.now()} uri={uri}/>])
-  }
-
-  function WelcomeScreen({ navigation }) {
+  const WelcomeScreen = ({ navigation }) => {
     return (
         <Welcome
           apiKey = {apiKey}
@@ -39,26 +33,16 @@ export default function App() {
           dimensions={windowDimensions} />
 )}
 
-  function HomeScreen({ navigation }) {
+  const HomeScreen = ({ navigation }) => {
     return(
         <View style={{
           backgroundColor:'#D6E6D6',
           height: windowDimensions.height,
           alignContent: "stretch",
         }}>
-          <Dashboard handleChange={handleChange} apiKey={apiKey} allImages={allImages} navigation={navigation} images={imageUris} dimensions={windowDimensions}/>
+          <Dashboard handleChange={handleChange} apiKey={apiKey} imageUris={imageUris} navigation={navigation} imageBaseStrings={imageBaseStrings} dimensions={windowDimensions}/>
           <StatusBar style="auto" />
         </View>
-    )
-  }
-
-  function ResponseScreen() {
-    return(
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ScrollView style={styles.scrollView}>
-          {allResultsCards}
-        </ScrollView>
-      </View>
     )
   }
 
@@ -79,34 +63,17 @@ export default function App() {
           }}
           component={WelcomeScreen} />
         <Stack.Screen 
-        name="Home"
-        options={{
-          headerStyle: {
-            backgroundColor:"#D6E6D6"
-          }
+          name="Home"
+          options={{
+            headerStyle: {
+              backgroundColor:"#D6E6D6"
+            }
         }}
-        component={HomeScreen} />
-        <Stack.Screen name="Results" component={ResponsePage} />
+          component={HomeScreen} />
+        <Stack.Screen 
+          name="Results" 
+          component={ResponsePage} />
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scrollView: {
-    maxHeight: 500,
-  },
-  button: {
-    backgroundColor: "#147d00",
-    justifyContent: "center",
-    height: 40,
-    width: 200,
-    fontSize: 30,
-  },
-})
